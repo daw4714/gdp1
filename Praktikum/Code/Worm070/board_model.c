938f429515efa3cf6570c3f0a7e1f911bc27839b
Some Student. Put your name here <your.email@your.provider>
2021-01-04 14:52:14 +0100

    Worm070

--- Praktikum/Code/Worm070/board_model.c
+++ Praktikum/Code/Worm070/board_model.c
@@ -6,106 +6,144 @@
 // Franz Regensburger
 // Ingolstadt University of Applied Sciences
 // (C) 2011
+//
+// The board model
 
 #include <curses.h>
 #include "worm.h"
 #include "board_model.h"
-#include "messages.h"
+#include "messages.h" // to use showDialog()
 
-// The board model
-void placeItem(struct board* aboard,int y, int x, enum BoardCodes board_code, chtype symbol, enum ColorPairs color_pair){
-  move(y,x);
-  attron(COLOR_PAIR(color_pair));
-  addch(SYMBOL_WORM_INNER_ELEMENT);
-  attroff(COLOR_PAIR(color_pair));
-  aboard->cells[y][x]= board_code;
-}
-//Getters
-int getLastRowOnBoard(struct board* aboard){
-  return aboard->last_row;
-}
-int getLastColOnBoard(struct board* aboard){
-  return aboard->last_col;
+// *************************************************
+// Placing and removing items from the game board
+// Check boundaries of game board
+// *************************************************
+
+// Place an item onto the curses display.
+void placeItem(struct board* aboard, int y, int x, enum BoardCodes board_code, chtype symbol, enum ColorPairs color_pair) {
+
+    //  Store item on the display (symbol code)
+    move(y, x);                         // Move cursor to (y,x)
+    attron(COLOR_PAIR(color_pair));     // Start writing in selected color
+    addch(symbol);                      // Store symbol on the virtual display
+    attroff(COLOR_PAIR(color_pair));    // Stop writing in selected color
+    aboard->cells[y][x] = board_code;   // Im Array cells abspeichern, dass an Position(y,x) board_code liegt
 }
 
+// Getters
+
+// Get the last usable row on the display
+int getLastRowOnBoard(struct board* aboard) {
+    return aboard->last_row;
+}
+
+// Get the last usable column on the display
+int getLastColOnBoard(struct board* aboard) {
+    return aboard->last_col; 
+}
+
+// Get Number of Food Items
 int getNumberOfFoodItems(struct board* aboard){
   return aboard->food_items;
 }
 
+//Get Codes for the Content at struct position
 enum BoardCodes getContentAt(struct board* aboard, struct pos position){
   return aboard->cells[position.y][position.x];
 }
 
+// Setters
+
 void setNumberOfFoodItems(struct board* aboard, int n){
   aboard->food_items = n;
 }
+
 void decrementNumberOfFoodItems(struct board* aboard){
-  aboard->food_items = aboard->food_items - 1;
+  aboard->food_items = aboard->food_items -1;
 }
 
+// Initializer
+
+// Board
 enum ResCodes initializeBoard(struct board* aboard){
-  // Check dimensions of the board
-if( COLS < MIN_NUMBER_OF_COLS || LINES < MIN_NUMBER_OF_ROWS + ROWS_RESERVED){
-  char buf[100];
-  sprintf(buf, "Das Fenster ist zu klein: wir brauchen %dx%d", MIN_NUMBER_OF_COLS, MIN_NUMBER_OF_ROWS + ROWS_RESERVED);
-showDialog(buf, "Bitte eine Taste drücken");
-  return RES_FAILED;
-}
-// Maximal index of a row
-aboard->last_row = MIN_NUMBER_OF_ROWS -1;
-// Maximal index of a column
-aboard->last_col = MIN_NUMBER_OF_COLS -1;
-return RES_OK;
+  //First we check the Dimesnosions of the Board again
+  if (COLS < MIN_NUMBER_OF_COLS ||LINES < MIN_NUMBER_OF_ROWS + ROWS_RESERVED){
+    char buf[100];
+    sprintf(buf, "Das Fenster ist zu klein: Wir brauchen %dx%d", MIN_NUMBER_OF_COLS, MIN_NUMBER_OF_ROWS+ROWS_RESERVED);
+    showDialog(buf, "Bitte eine Taste drücken");
+    return RES_FAILED;
+  }
+  // Maximal index of a row
+  aboard->last_row = MIN_NUMBER_OF_ROWS -1;
+  // Maximal index of a column
+  aboard->last_col = MIN_NUMBER_OF_COLS -1;
+  return RES_OK;
 }
 
-enum ResCodes initializeLevel(struct board * aboard) {
+// Level
 
-// define local variables for loops etc
-// Fill board and screen buffer with empty cells.
-for (int y = 0; y <= aboard->last_row ; y++) {
-for (int x = 0; x <= aboard->last_col ; x++) {
-placeItem(aboard,y,x,BC_FREE_CELL,SYMBOL_FREE_CELL,COLP_FREE_CELL);
-}
-}
-// Draw a line in order to separate the message area
-// Note: we cannot use function placeItem() since the message area
-// is outside the board!
-int y = aboard->last_row + 1;
-for (int x=0; x <= aboard->last_col ; x++) {
-move(y, x);
-attron(COLOR_PAIR(COLP_BARRIER));
-addch(SYMBOL_BARRIER);
-attroff(COLOR_PAIR(COLP_BARRIER));
-}
-// Draw a line to signal the rightmost column of the board.
-for (y=0; y <= aboard->last_row ; y++) {
-placeItem(aboard,y,aboard->last_col,
-BC_BARRIER,SYMBOL_BARRIER,COLP_BARRIER);
-}
-// Barriers: use a loop
-for (y = aboard->last_row /2; y < (aboard->last_row /2) +11; y++  ) {
-int x = (aboard->last_col /3) - 5;
-placeItem(aboard,y,x,BC_BARRIER,SYMBOL_BARRIER,COLP_BARRIER);
-}
-for (y = aboard->last_row /3; y <  (aboard->last_row /3) +11; y++ ) {
-int x = (aboard-> last_col / 2) + 7;
-placeItem(aboard,y,x,BC_BARRIER,SYMBOL_BARRIER,COLP_BARRIER);
-}
-// Food
-placeItem(aboard, 3, 3,BC_FOOD_1,SYMBOL_FOOD_1,COLP_FOOD_1);
-placeItem(aboard, 5, 18,BC_FOOD_1,SYMBOL_FOOD_1,COLP_FOOD_1);
-placeItem(aboard, 13, 3,BC_FOOD_2,SYMBOL_FOOD_2,COLP_FOOD_2);
-placeItem(aboard, 3, 13,BC_FOOD_2,SYMBOL_FOOD_2,COLP_FOOD_2);
-placeItem(aboard, 18, 3,BC_FOOD_2,SYMBOL_FOOD_2,COLP_FOOD_2);
-placeItem(aboard, 3,17,BC_FOOD_2,SYMBOL_FOOD_2,COLP_FOOD_2);
-placeItem(aboard, 15, 13,BC_FOOD_3,SYMBOL_FOOD_3,COLP_FOOD_3);
-placeItem(aboard, 10, 10,BC_FOOD_3,SYMBOL_FOOD_3,COLP_FOOD_3);
-placeItem(aboard, 8, 13,BC_FOOD_3,SYMBOL_FOOD_3,COLP_FOOD_3);
-placeItem(aboard, 18, 10,BC_FOOD_3,SYMBOL_FOOD_3,COLP_FOOD_3);
+enum ResCodes initializeLevel(struct board* aboard) {
+  int y;
+  int x;
+  //Fill board and screen buffer with empty cells
+  for (y=0; y <= aboard->last_row; y++){
+    for (x=0; x <= aboard->last_col; x++){
+      placeItem(aboard, y, x, BC_FREE_CELL, SYMBOL_FREE_CELL, COLP_FREE_CELL);
+    }
+  }
+  // Seperate the Message area onscreen (Use of placeItem() not allowed (outside board)
+  y = aboard->last_row +1;
+  for(x=0; x <= aboard->last_col; x++){
+    move(y,x);
+    attron(COLOR_PAIR(COLP_BARRIER));
+    addch(SYMBOL_BARRIER);
+    attroff(COLOR_PAIR(COLP_BARRIER));
+  }
+  // Line at the rightmost colum of board
+  for(y=0; y <= aboard->last_row; y++){
+    placeItem(aboard, y, aboard->last_col, BC_BARRIER, SYMBOL_BARRIER, COLP_BARRIER);
+  }
+  // Barriers
+  x = (aboard->last_col) / 3 - 2;
+  for(y=3; y < 13; y++){
+    placeItem(aboard, y, x, BC_BARRIER, SYMBOL_BARRIER, COLP_BARRIER);
+  }
+  x = (aboard->last_col) *2 /3 ;
+  for(y= 8; y < 18; y++){
+    placeItem(aboard, y, x, BC_BARRIER, SYMBOL_BARRIER, COLP_BARRIER);
+  }
+  // Food
+  placeItem(aboard, 3, 3,  BC_FOOD_1, SYMBOL_FOOD_1, COLP_FOOD_1);
+  placeItem(aboard, 23, 11,  BC_FOOD_1, SYMBOL_FOOD_1, COLP_FOOD_1);
+  placeItem(aboard, 4, 10, BC_FOOD_2, SYMBOL_FOOD_2, COLP_FOOD_2);
+  placeItem(aboard, 10, 3, BC_FOOD_2, SYMBOL_FOOD_2, COLP_FOOD_2);
+  placeItem(aboard, 7, 9, BC_FOOD_2, SYMBOL_FOOD_2, COLP_FOOD_2);
+  placeItem(aboard, 5, aboard->last_col-4, BC_FOOD_2, SYMBOL_FOOD_2, COLP_FOOD_2);
+  placeItem(aboard, 6, 6, BC_FOOD_3, SYMBOL_FOOD_3, COLP_FOOD_3);
+  placeItem(aboard, 5, 7, BC_FOOD_3, SYMBOL_FOOD_3, COLP_FOOD_3);
+  placeItem(aboard, 9, 4, BC_FOOD_3, SYMBOL_FOOD_3, COLP_FOOD_3);
+  placeItem(aboard, 8, 1, BC_FOOD_3, SYMBOL_FOOD_3, COLP_FOOD_3);
+  
 
-// Initialize number of food items
-// Attention: must match number of items placed on the board above
-aboard->food_items = 10;
-return RES_OK;
-} 
+
+  // Initialize number of Food Items
+  aboard->food_items = 10;
+  return RES_OK;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- Praktikum/Code/Worm070/worm_model.c
+++ Praktikum/Code/Worm070/worm_model.c
@@ -6,211 +6,209 @@
 // Franz Regensburger
 // Ingolstadt University of Applied Sciences
 // (C) 2011
+//
+// The worm model
+
 #include <curses.h>
 #include "worm.h"
 #include "board_model.h"
 #include "worm_model.h"
 
-//Getters
-struct pos getWormHeadPos(struct worm* aworm){
-  //Structure are passed by value!
-  // -> we return a copy here
-  return aworm->wormpos[aworm->headindex];
-}
+//*****************************************************************
+// Data defining the worm
+//*****************************************************************
+enum ColorPairs theworm_wcolor; //  Code of color pair used for the worm 
 
-int getWormLength(struct worm* aworm){
-  return aworm->cur_lastindex - aworm->headindex;
-}
-
-// The worm model
+// *****************************************************
 // Functions concerning the management of the worm data
-enum ResCodes initializeWorm(struct worm* aworm, int len_max, int len_cur, struct pos headpos, enum WormHeading dir, enum ColorPairs color) {//d enum ersetzt int
-  // Local variables for loops etc.
-  int i = 1;
-  // Initialize las usable index to len_max -1
-  // (aworm*).maxindex
-  aworm->maxindex = len_max -1;
-  //Current last usable index in array. May grow upto maxindex
-  aworm->cur_lastindex = len_cur -1;
-  // Initialize headindex
-  // (aworm*).headindex
-  aworm->headindex = 0 ;
+// *****************************************************
 
-  //Mark all elements as unused in the array of positions
-  //(aworm*).wormpos_y[] and (aworm*).wormpos_x[]
-  //An unused position in the array is marked
-  //with code UNUSED_POS_ELEMENT
-  for(i = 0; i <= aworm->maxindex; i++){
-    aworm->wormpos[i].y = UNUSED_POS_ELEM;
-    aworm->wormpos[i].x = UNUSED_POS_ELEM;
-//    i = i +1;
+// Growing of the Worm
+void growWorm(struct worm* aworm, enum Boni growth){
+  if (aworm->cur_lastindex + growth <= aworm->maxindex){
+    aworm->cur_lastindex += growth;
+  } 
+  else {
+    aworm->cur_lastindex = aworm->maxindex;  
   }
-  // Initialize position of worms head
-  aworm->wormpos[aworm->headindex] = headpos;
-  // Initialize the heading of the worm
-  setWormHeading(aworm, dir);
-  // Initialze color of the worm
-  aworm->wcolor = color;
+}
 
-  return RES_OK;
+//Getters
+int getWormLength(struct worm* aworm){
+  return aworm->cur_lastindex + 1;
+}
+
+struct pos getWormHeadPos(struct worm* aworm){
+    return aworm->wormpos[aworm->headindex];
+}
+
+// Initialize the worm
+extern enum ResCodes initializeWorm(struct worm* aworm, int len_max, int len_cur, struct pos headpos, enum WormHeading dir, enum ColorPairs color) {
+    //Local variables for loops etc.
+    int array_index_iterator;
+    
+    //Initialize last usable index to len_max -1
+    aworm->maxindex = len_max -1;
+    // Current Last usable index (may grow up to maxindex)
+    aworm->cur_lastindex = len_cur -1;
+    
+    //Initialize headindex
+    aworm->headindex = 0;
+    
+    // Marking of unused positions in the array with code UNUSED_POS_ELEM
+    for (array_index_iterator = 1; array_index_iterator <= aworm->maxindex; array_index_iterator++){
+      aworm->wormpos[array_index_iterator].y = UNUSED_POS_ELEM;
+      aworm->wormpos[array_index_iterator].x = UNUSED_POS_ELEM;
+    }
+
+    // Initialize the positions of the head of the worm
+    aworm->wormpos[aworm->headindex] = headpos;
+    
+    // Initialize the heading of the worm
+    setWormHeading(aworm, dir);
+
+    // Initialze color of the worm
+    aworm->wcolor = color;
+
+    return RES_OK;
 }
 
 // Show the worms's elements on the display
 // Simple version
-void showWorm(struct board* aboard,struct worm* aworm) {
-  // Due to our encoding we just need to show the head element
-  // All ot
-  // her elements are already displayed
-
-int i = aworm->headindex + 1;
-  do{
-   if(i == aworm->headindex){
+extern void showWorm(struct board* aboard, struct worm* aworm) {
+    // Due to our encoding we just need to show the head element
+    // All other elements are already displayed
     placeItem(aboard,
-      aworm->wormpos[aworm->headindex].y ,
-      aworm->wormpos[aworm->headindex].x, BC_USED_BY_WORM,
-      SYMBOL_WORM_HEAD_ELEMENT,aworm->wcolor);
+            aworm->wormpos[aworm->headindex].y,
+            aworm->wormpos[aworm->headindex].x,
+            BC_USED_BY_WORM,
+            SYMBOL_WORM_HEAD_ELEMENT,
+            aworm->wcolor);
 
-   }  
-     placeItem(aboard,
-      aworm->wormpos[aworm->headindex].y ,
-      aworm->wormpos[aworm->headindex].x, BC_USED_BY_WORM,
-      SYMBOL_WORM_INNER_ELEMENT,aworm->wcolor);
-
-      i++;
-      if(i>(aworm->cur_lastindex-1)){
-        i = aworm->headindex;
-         placeItem(aboard,
-      aworm->wormpos[aworm->headindex].y ,
-      aworm->wormpos[aworm->headindex].x, BC_USED_BY_WORM,
-      SYMBOL_WORM_TAIL_ELEMENT,aworm->wcolor);
-
+    int tailindex = (aworm->headindex+1) % (aworm->cur_lastindex+1);
+    placeItem(aboard,
+            aworm->wormpos[tailindex].y,
+            aworm->wormpos[tailindex].x,
+            BC_USED_BY_WORM,
+            SYMBOL_WORM_TAIL_ELEMENT,
+            aworm->wcolor);
+    for (int i = aworm->headindex -1; i != tailindex; i--){
+      if (i<0){
+        i = aworm->cur_lastindex+1;
+        continue;
       }
-    
+      placeItem(aboard,
+            aworm->wormpos[i].y,
+            aworm->wormpos[i].x,
+            BC_USED_BY_WORM,
+            SYMBOL_WORM_INNER_ELEMENT,
+            aworm->wcolor);
 
-  }while(i != aworm->headindex);
+    } 
+
 }
 
-void growWorm(struct worm * aworm, enum Boni growth) {
-// Play it safe and inhibit surpassing the bound
-if (aworm->cur_lastindex + growth <= aworm->maxindex) {
-aworm->cur_lastindex += growth;
-} else {
-aworm->cur_lastindex = aworm->maxindex;
-}
-}
-
-void moveWorm(struct board* aboard, struct worm* aworm, enum GameStates* agame_state) {//d enum ersetzt int
- struct pos headpos;
-
-// Get the current position of the worm's head element
-headpos = aworm->wormpos[aworm->headindex];
-// Compute new head position according to current heading.
-// Do not store the new head position in the array of positions, yet.
-headpos.x += aworm->dx;
-headpos.y += aworm->dy;
-
-  // Check if we would hit something (good or bad) or are going to leave
-  /// the display if we move the worm's head according to worm's last
-  // We are not allowed to leave the display's window.
-  if (headpos.x < 0) {
-    *agame_state = WORM_OUT_OF_BOUNDS;
-  } else if (headpos.x > getLastColOnBoard(aboard) ) {
-    *agame_state = WORM_OUT_OF_BOUNDS;
-  } else if (headpos.y < 0) {
-    *agame_state = WORM_OUT_OF_BOUNDS;
-  } else if (headpos.y > getLastRowOnBoard(aboard) ) {
-    *agame_state = WORM_OUT_OF_BOUNDS;
-  } else {
-    // We will stay within bounds.
- // Check if the worms head hits any items at the new position on the board.
-// Hitting food is good, hitting barriers or worm elements is bad.
-switch ( getContentAt(aboard,headpos) ) {
-case BC_FOOD_1:
-*agame_state = WORM_GAME_ONGOING;
-// Grow worm according to food item digested
-growWorm(aworm, BONUS_1);
-decrementNumberOfFoodItems(aboard);
-break;
-case BC_FOOD_2:
-*agame_state = WORM_GAME_ONGOING;
-// Grow worm according to food item digested
-growWorm(aworm, BONUS_2);
-decrementNumberOfFoodItems(aboard);
-break;
-
-case BC_FOOD_3:
-*agame_state = WORM_GAME_ONGOING;
-// Grow worm according to food item digested
-growWorm(aworm, BONUS_3);
-decrementNumberOfFoodItems(aboard);
-break;
-
-case BC_BARRIER:
-// That's bad: stop game
-*agame_state = WORM_CRASH;
-break;
-case BC_USED_BY_WORM:
-// That's bad: stop game
-*agame_state = WORM_CROSSING;
-break;
-default:
-// Without default case we get a warning message.
-{;} // Do nothing. C syntax dictates some statement, here.
-}
-}
-
-    
-  //Check the status of *agame_state
-  //Go on if nothing bad happend
-  if( *agame_state == WORM_GAME_ONGOING ){
-    //So all is well: we did not hit anything bad and did not leave the
-    //window. --> Update the worm structure.
-    //Increment theworm_headindex
-    //Go round if end of worm is reached (ring buffer)
-aworm->headindex++;
-if (aworm->headindex > aworm->cur_lastindex) {
-aworm->headindex = 0;
-}
-// Store new coordinates of head element in worm structure
-aworm->wormpos[aworm->headindex] = headpos;
-  }
-}
-
-
-// Setters
-void setWormHeading(struct worm* aworm, enum WormHeading dir) {
-  switch(dir) {
-    case WORM_UP :// User wants up
-      aworm->dx=0;
-      aworm->dy=-1;
-      break;
-    case WORM_DOWN :// User wants down
-      aworm->dx=0;
-      aworm->dy=1;
-      break;
-    case WORM_LEFT :// User wants left
-      aworm->dx=-1;
-      aworm->dy=0;
-      break;
-    case WORM_RIGHT :// User wants right
-      aworm->dx=1;
-      aworm->dy=0;
-      break;
-  }
-}
-void cleanWormTail(struct board* aboard, struct worm* aworm) {
+// Remove the worm's elements from the display
+extern void cleanWormTail(struct board* aboard, struct worm* aworm){
   int tailindex;
 
   //Compute tailindex
-  tailindex = (aworm->headindex + 1) % aworm->cur_lastindex;
-  //Check the array of worm elements.
-  //Is the array element at tailindex already in use?
-  //Checking either array theworm_wormpos_y
-  //or theworm_wormpos_x is enough.
+  tailindex = (aworm->headindex + 1) % (aworm->cur_lastindex + 1);
 
-  if(aworm->wormpos[tailindex].x != UNUSED_POS_ELEM){
-    //YES: place a SYMBOL_FREE_CELL at the tail's position
-    placeItem(aboard, aworm->wormpos[tailindex].y, aworm->wormpos[tailindex].x, BC_FREE_CELL,  SYMBOL_FREE_CELL, COLP_FREE_CELL);
+  //Check if array element of tailindex is in use
+  if (aworm->wormpos[tailindex].y != UNUSED_POS_ELEM) {
+    placeItem(aboard, aworm->wormpos[tailindex].y, aworm->wormpos[tailindex].x, BC_FREE_CELL, SYMBOL_FREE_CELL, COLP_FREE_CELL);
   }
-} 
+}
 
+extern void moveWorm(struct board* aboard, struct worm* aworm, enum GameStates* agame_state) {
+    struct pos headpos;
+    headpos = aworm->wormpos[aworm->headindex];
+    //int headpos_y;
+    //int headpos_x;
+    // Compute und store new head position according to current heading.
+    headpos.x += aworm->dx;
+    headpos.y += aworm->dy;
+
+    // Check if we would leave the display if we move the worm's head according
+    // to worm's last direction.
+    // We are not allowed to leave the display's window.
+    if (headpos.x < 0) {
+        *agame_state = WORM_OUT_OF_BOUNDS;                 //links aus dem Bild
+    } else if (headpos.x > getLastColOnBoard(aboard)) { 
+        *agame_state = WORM_OUT_OF_BOUNDS;                 //rechts aus dem Bild
+    } else if (headpos.y < 0) {  
+        *agame_state = WORM_OUT_OF_BOUNDS;                 //oben aus dem Bild    
+	} else if (headpos.y > getLastRowOnBoard(aboard)) {
+        *agame_state = WORM_OUT_OF_BOUNDS;                 //unten aus dem Bild
+    } else {
+        // We will stay within bounds.
+        // Check if the worm's head hits something
+        switch(getContentAt(aboard,headpos)){  
+          case BC_FOOD_1:
+            *agame_state = WORM_GAME_ONGOING;
+            // Grow accordingly
+            growWorm(aworm, BONUS_1);
+            decrementNumberOfFoodItems(aboard);
+            break;
+          case BC_FOOD_2:
+            *agame_state = WORM_GAME_ONGOING;
+            // Grow accordingly
+            growWorm(aworm, BONUS_2);
+            decrementNumberOfFoodItems(aboard);
+            break;
+          case BC_FOOD_3:
+            *agame_state = WORM_GAME_ONGOING;
+            // Grow accordingly
+            growWorm(aworm, BONUS_3);
+            decrementNumberOfFoodItems(aboard);
+            break; 
+          case BC_BARRIER:
+            //Stop game            
+            *agame_state = WORM_CRASH;
+            break;
+          case BC_USED_BY_WORM:
+            //Stop game            
+            *agame_state = WORM_CROSSING;
+            break;
+          default:
+            {;} // Do nothing
+        }
+    }
+
+    //Check the status of *agame_state
+    //Go on if nothing happened
+    if (*agame_state == WORM_GAME_ONGOING) {
+      // Ring Buffer to increase Headindex
+      aworm->headindex++;
+      if (aworm->headindex > aworm->cur_lastindex) {
+        aworm->headindex=0;
+      }
+      //Store new coordinates of head element in worm structure
+      aworm->wormpos[aworm->headindex] = headpos;
+    }
+}
+
+
+
+// Setters
+extern void setWormHeading(struct worm* aworm, enum WormHeading dir) {
+    switch(dir) {
+        case WORM_UP :// User wants up
+            aworm->dx=0;
+            aworm->dy=-1;
+            break;
+        case WORM_DOWN :// User wants down
+            aworm->dx=0;
+            aworm->dy=1;
+            break;
+        case WORM_LEFT :// User wants left
+            aworm->dx=-1;
+            aworm->dy=0;
+            break;
+        case WORM_RIGHT      :// User wants right
+            aworm->dx=1;
+            aworm->dy=0;
+            break;
+    }
+}
